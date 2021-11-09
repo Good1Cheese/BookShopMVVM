cf7c7421dc402f6083ff58806ed50366c79d3d72
GuterKase <2916675@gmail.com>
2021-11-09 19:19:27 +0900

    добавлен дизайн основого меню

---
+++ BookShopMVVM/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace BookShopMVVM
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
