72f42eb3bc042e47c3ebf21b4b40a995e6e4bdc9
GuterKase <2916675@gmail.com>
2021-12-02 19:54:28 +0900

    подредачил код, обновил дизайн, добавил корзину и возможность убирать из неё товары

--- BookShopMVVM/ViewModels/ShopViewModel.cs
+++ BookShopMVVM/ViewModels/ShopViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using BookShopMVVM.Commands;
 using BookShopMVVM.Models;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace BookShopMVVM.ViewModels
 {
@@ -28,30 +30,27 @@ namespace BookShopMVVM.ViewModels
                 new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
                 new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231)
             };
+
+            AddProductToCart = new ActionCommand((obj) =>
+            {
+                CartProducts.Add((Product)obj);
+            });
+
+            RemoveProduct = new ActionCommand((obj) =>
+            {
+                CartProducts.Remove((Product)obj);
+            });
         }
 
         public ShopViewModel()
         {
-            Products = new List<Product>()
-            {
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231),
-                new Product("Product Name", "Nullam posuere turpis vel lacinia luctus. Donec in efficitur neque.", 231)
-            };
+            
         }
 
+        public ActionCommand AddProductToCart { get; set; }
+        public ActionCommand RemoveProduct { get; set; }
+
         public List<Product> Products { get; set; }
+        public ObservableCollection<Product> CartProducts { get; set; } = new ObservableCollection<Product>();
     }
 }
